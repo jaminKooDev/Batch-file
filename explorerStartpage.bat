de71aac99c1fa696453061a81122d3a0a78c8bfd
jaminKooDev <66311438+jaminKooDev@users.noreply.github.com>
2020-07-13 17:32:20 +0900

    Update explorerStartpage.bat

--- explorerStartpage.bat
+++ explorerStartpage.bat
@@ -1,3 +1,3 @@
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Start Page" /t "REG_SZ" /d "https://www.naver.com/" /f
 
-del %systemdrive%\users\%username%\desktop\explorerStartpage.bat
+del .\explorerStartpage.bat
